b3522b03ee96f5dedde61ff21068269889aa4fa5
kannonanayuu <yb270186-1716@tbz.t-com.ne.jp>
2019-04-10 10:20:27 +0000

    edit

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -3,7 +3,7 @@ class RanksController < ApplicationController
   def index
     if params[:id] == "301" then
       @answers = Answer.where(year: 30).where(jirei: 1)        
-        
+      @id = params[:id] 
     end
   end
 
