d3dba675558cdf5ff302b5e35c323e355d2537e2
kannonanayuu <yb270186-1716@tbz.t-com.ne.jp>
2019-05-01 11:10:10 +0000

    edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,172 @@ class CommentsController < ApplicationController
     if params[:id] == "301" then
       @answers = Answer.where.not(user_id: current_user.id).where(year: 30).where(jirei: 1).includes(:comments)
       @id = params[:id]
+    elsif params[:id] == "302" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 30).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "303" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 30).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "304" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 30).where(jirei: 4).includes(:comments)
+      @id = params[:id]        
+      
+    elsif params[:id] == "291" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 29).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "292" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 29).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "293" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 29).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "294" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 29).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
 
+    elsif params[:id] == "281" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 28).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "282" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 28).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "283" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 28).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "284" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 28).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+
+    elsif params[:id] == "271" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 27).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "272" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 27).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "273" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 27).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "274" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 27).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+      
+    elsif params[:id] == "261" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 26).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "262" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 26).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "263" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 26).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "264" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 26).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+ 
+    elsif params[:id] == "251" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 25).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "252" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 25).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "253" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 25).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "254" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 25).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+ 
+    elsif params[:id] == "241" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 24).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "242" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 24).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "243" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 24).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "244" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 24).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+ 
+    elsif params[:id] == "231" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 23).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "232" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 23).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "233" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 23).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "234" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 23).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+ 
+    elsif params[:id] == "221" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 22).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "222" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 22).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "223" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 22).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "224" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 22).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+ 
+    elsif params[:id] == "211" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 21).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "212" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 21).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "213" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 21).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "214" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 21).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+ 
+    elsif params[:id] == "201" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 20).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "202" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 20).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "203" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 20).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "204" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 20).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+ 
+    elsif params[:id] == "191" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 19).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "192" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 19).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "193" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 19).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "194" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 19).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+       
+    elsif params[:id] == "181" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 18).where(jirei: 1).includes(:comments)
+      @id = params[:id]
+    elsif params[:id] == "182" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 18).where(jirei: 2).includes(:comments)
+      @id = params[:id]  
+    elsif params[:id] == "183" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 18).where(jirei: 3).includes(:comments)
+      @id = params[:id]        
+    elsif params[:id] == "184" then
+      @answers = Answer.where.not(user_id: current_user.id).where(year: 18).where(jirei: 4).includes(:comments)
+      @id = params[:id]  
+ 
     end
     
     
