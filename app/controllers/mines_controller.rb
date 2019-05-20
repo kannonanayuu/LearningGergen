adc694adbba7c105637f9b52794648a9fc2cff67
kannonanayuu <yb270186-1716@tbz.t-com.ne.jp>
2019-05-20 11:20:53 +0000

    edit

--- app/controllers/mines_controller.rb
+++ app/controllers/mines_controller.rb
@@ -3,6 +3,7 @@ class MinesController < ApplicationController
   def index
     @usr = current_user.name
     @answers = Answer.where(user_id: current_user.id)
+    
     @drills = Drill.where(user_id: current_user.id)
     @titles = Title.where(user_id: current_user.id)
   end
