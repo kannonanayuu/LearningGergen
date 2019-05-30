d74028fe8d58a4bfb74e1350a476696aee28c4a8
kannonanayuu <yb270186-1716@tbz.t-com.ne.jp>
2019-05-30 11:27:43 +0000

    edit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,58 +5,58 @@ class ReviewsController < ApplicationController
 
   def index
     if params[:id] == "131" then
-      @answers = Drill.where.not(id: 1).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 1).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]
     elsif params[:id] == "132" then
-      @answers = Drill.where.not(id: 2).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 2).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "133" then
-      @answers = Drill.where.not(id: 3).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 3).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "141" then
-      @answers = Drill.where.not(id: 4).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 4).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "142" then
-      @answers = Drill.where.not(id: 5).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 5).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "211" then
-      @answers = Drill.where.not(id: 6).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 6).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "212" then
-      @answers = Drill.where.not(id: 7).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 7).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "213" then
       @answers = Drill.where.not(id: 8).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "214" then
-      @answers = Drill.where.not(id: 9).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 9).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "215" then
-      @answers = Drill.where.not(id: 10).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 10).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "216" then
-      @answers = Drill.where.not(id: 11).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 11).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "221" then
-      @answers = Drill.where.not(id: 12).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 12).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "222" then
-      @answers = Drill.where.not(id: 13).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 13).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "223" then
-      @answers = Drill.where.not(id: 14).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 14).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "226" then
-      @answers = Drill.where.not(id: 15).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 15).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "227" then
-      @answers = Drill.where.not(id: 16).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 16).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "232" then
-      @answers = Drill.where.not(id: 17).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 17).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     elsif params[:id] == "233" then
-      @answers = Drill.where.not(id: 18).where.not(user_id: current_user.id).includes(:reviews)
+      @answers = Drill.where(id: 18).where.not(user_id: current_user.id).includes(:reviews)
       @id = params[:id]      
     end
     
@@ -89,6 +89,42 @@ class ReviewsController < ApplicationController
     jdg.drill_id = @@did
 
     if(jdg.strt.present? or  jdg.s.present? or  jdg.w.present? or jdg.o.present? or jdg.o.present? or jdg.t.present?  or jdg.j1.present? or jdg.j2.present? or jdg.j3.present? or jdg.j4.present?)then
+      if jdg.good.nil?
+        jdg.good =0
+      end
+      
+      if jdg.good2.nil?
+        jdg.good2 =0
+      end  
+     
+      if jdg.good3.nil?
+        jdg.good3 =0
+      end
+    
+      if jdg.good4.nil?
+        jdg.good4 =0
+      end
+     
+      if jdg.good5.nil?
+        jdg.good5 =0
+      end
+     
+      if jdg.good6.nil?
+        jdg.good6 =0
+      end 
+    
+      if jdg.good7.nil?
+        jdg.good7 =0
+      end 
+    
+      if jdg.good8.nil?
+        jdg.good8 =0
+      end 
+    
+      if jdg.good9.nil?
+        jdg.good9 =0
+      end 
+      
       jdg.save
       redirect_to  drill_reviews_path(jdg.drill_id)  
     else
@@ -108,7 +144,7 @@ class ReviewsController < ApplicationController
   
   private
   def review_params
-    params.require(:review).permit(:id, :drill_id, :user_id, :strt, :s, :w, :o, :t, :j1, :j2, :j3, :j4, :good, :bad , :good2, :bad2, :good3, :bad3, :good4, :bad4, :good5, :bad5, :good6, :bad6, :good7, :bad7, :good8, :bad8, :good9, :bad9 )
+    params.require(:review).permit(:id, :drill_id, :user_id, :strt, :s, :w, :o, :t, :j1, :j2, :j3, :j4, :good, :good2, :good3, :good4, :good5, :good6, :good7, :good8, :good9 )
   end
 
   
